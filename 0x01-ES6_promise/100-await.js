2bd7924d4bf5b2e309024c1680c603dce3a90af4
Miracle Obinna <mmrealworld@yahoo.com>
2024-03-06 15:19:44 -0800

    update

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,18 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    // Execute uploadPhoto and createUser concurrently
-    const [photoResponse, userResponse] = await Promise.all([
-      uploadPhoto(),
-      createUser()
-    ]);
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
 
-    return { photo: photoResponse, user: userResponse };
+    return ({ photo: photoResponse, user: userResponse });
   } catch (error) {
-    // Handle errors from uploadPhoto and createUser separately
-    const photoError = error instanceof uploadPhoto.Error ? error : null;
-    const userError = error instanceof createUser.Error ? error : null;
-
-    return { photo: photoError, user: userError };
+    return ({ photo: null, user: null });
   }
 }
