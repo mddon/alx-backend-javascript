bc211a596a563d676f9a940863421ba3f8896ac9
Miracle Obinna <mmrealworld@yahoo.com>
2024-03-06 10:21:46 -0800

    update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => { console.log('Got a response from the API'); });
 }
+export default function handleResponseFromAPI;
