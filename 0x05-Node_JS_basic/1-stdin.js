d855d35ac393e842c028795d75298860b47a6053
mddon <mmrealworld@yahoo.com>
2024-04-30 14:37:56 -0700

    njs_basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
-  process.stdin.on('info', (info) => {
-    process.stdout.write(`Your name is: ${info.toString()}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
-  process.stdin.on('info', (info) => {
-    process.stdout.write(`Your name is: ${info.toString()}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
   process.on('exit', () => {
